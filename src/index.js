4c290996352bda83486fd044c397f96644a0d09f
Apon Palanuwech <palaa159@newschool.edu>
2017-08-23 16:37:19 +0700

    - WIP

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    var NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp />,
+      document.getElementById('root')
+    );
+  });
+}
+
+registerServiceWorker();
